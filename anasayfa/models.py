b902d786344cd2105cfdfed5f1b194d54b85376c
Ozan Alp Sarıdoğan <ozansardogan379@gmail.com>
2025-08-27 09:38:18 +0300

    video ekleme özelliği

--- anasayfa/models.py
+++ anasayfa/models.py
@@ -14,6 +14,7 @@ class AyasBilgi(models.Model):
     baslik = models.CharField("Başlık", max_length=200)
     aciklama = models.TextField("Açıklama")
     resim = models.ImageField("Görsel", upload_to='ayas/', blank=True, null=True)
+    video = models.FileField("Ayas Videosu", upload_to='ayas/videos/', blank=True, null=True)
     tarih = models.DateField("Tarih", auto_now_add=True)
 
     def __str__(self):
@@ -27,6 +28,7 @@ class Etkinlik(models.Model):
     saat = models.TimeField("Saat")
     yer = models.CharField("Yer", max_length=200)
     resim = models.ImageField("Etkinlik Görseli", upload_to='etkinlikler/', blank=True, null=True)
+    video = models.FileField("Etkinlik Videosu", upload_to='etkinlikler/videos/', blank=True, null=True)
     olusturma_tarihi = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -37,6 +39,7 @@ class Duyuru(models.Model):
     aciklama = models.TextField("Açıklama")
     tarih = models.DateField("Tarih", auto_now_add=True)
     resim = models.ImageField("Duyuru Görseli", upload_to='duyurular/', blank=True, null=True)
+    video = models.FileField("Duyuru Videosu", upload_to='duyurular/videos/', blank=True, null=True)
 
     def __str__(self):
         return self.baslik
@@ -45,6 +48,7 @@ class FaydaliBilgi(models.Model):
     baslik = models.CharField("Başlık", max_length=200)
     aciklama = models.TextField("Açıklama")
     resim = models.ImageField("Görsel", upload_to='faydali_bilgiler/', blank=True, null=True)
+    video = models.FileField("Faydalı Bilgi Videosu", upload_to='faydali_bilgiler/videos/', blank=True, null=True)
     tarih = models.DateField("Tarih", auto_now_add=True)
 
     def __str__(self):
