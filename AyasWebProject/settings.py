6adad2e3687f5d2e7fadf6c4863742c9d4b94e61
Ozan Alp Sarıdoğan <ozansardogan379@gmail.com>
2025-10-26 17:16:15 +0300

    .

--- AyasWebProject/settings.py
+++ AyasWebProject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ayaswebproject-test.onrender.com', '127.0.0.1', 'localhost', 'www.ayasvakfi.com', 'ayasvakfi.com']
 
